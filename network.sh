5d81bdbdd65ec2f67b0d8df0a55235fb28af0916
pwwpcheng <pwwp@outlook.com>
2016-04-18 15:15:30 +0800

    Hostname modified

--- network.sh
+++ network.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 set -x
 
-# Install type includes: Controller | Network | Compute
+# Install type includes: Controller | Network | Compute | Storage
 export INSTALL_TYPE=Network
 
 # Install confirmation
-read -p "Are you sure to install OpenStack Network Node? [Y/N] " YN
+read -p "Are you sure to install OpenStack Compute Node? [Y/N] " YN
 if [ "$YN" != "Y" ] && [ "$YN" != "y" ]; then
-	echo "Aborted."
-	exit 1
+        echo "Aborted."
+        exit 1
 fi
+
+# define functions
+[ -e ${PWD}/functions ] || exit 1
+source ${PWD}/export.env
+. ${PWD}/functions
+
+set_env
+
 
 if ! [ -z "${INSTALL_CORE}" ]; then
-	# define functions
-	[ -e ${PWD}/functions ] || exit 1 
-	. ${PWD}/functions
+        # Setup OpenStack Packages
+        set_hosts
+        inst_ntp
+        inst_openstack
 
-	# Import environment variables
-	set_env
-	set_hosts
-
-	# Setup OpenStack Packages
-	set_hosts
-	inst_ntp
-	inst_openstack
-
-	# Setup compute service as network node
-	add_networking
+        # Setup compute service as network node
+        add_networking
+        create_init_network
 fi
 
-# Create a initial network
-create_init_network
+
